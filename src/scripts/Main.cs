a47edaafb83767f8d474c60594e64a85c22aa21e
RuanSR <ruansilvarosa@gmail.com>
2023-06-10 11:37:57 -0300

    created main game scene and script
    signal player fixed

---
+++ src/scripts/Main.cs
@@ -0,0 +1,75 @@
+using Godot;
+using System;
+
+public class Main : Node
+{
+    [Export]
+    public PackedScene MobScene;
+
+    public int Score;
+    public override void _Ready()
+    {
+        GD.Randomize();
+        NewGame();
+    }
+
+    public void GameOver()
+    {
+        GetNode<Timer>("MobTimer").Stop();
+        GetNode<Timer>("ScoreTimer").Stop();
+    }
+
+    public void NewGame()
+    {
+        Score = 0;
+
+        var player = GetNode<Player>("Player");
+        var startPosition = GetNode<Position2D>("StartPosition");
+        player.Start(startPosition.Position);
+
+        GetNode<Timer>("StartTimer").Start();
+    }
+
+    public void OnScoreTimerTimeout()
+    {
+        Score++;
+    }
+
+    public void OnStartTimerTimeout()
+    {
+        GetNode<Timer>("MobTimer").Start();
+        GetNode<Timer>("ScoreTimer").Start();
+    }
+
+    public void OnMobTimerTimeout()
+    {
+        // Note: Normally it is best to use explicit types rather than the `var`
+        // keyword. However, var is acceptable to use here because the types are
+        // obviously Mob and PathFollow2D, since they appear later on the line.
+
+        // Create a new instance of the Mob scene.
+        var mob = (Mob)MobScene.Instance();
+
+        // Choose a random location on Path2D.
+        var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+        mobSpawnLocation.Offset = GD.Randi();
+
+        // Set the mob's direction perpendicular to the path direction.
+        float direction = mobSpawnLocation.Rotation + Mathf.Pi / 2;
+
+        // Set the mob's position to a random location.
+        mob.Position = mobSpawnLocation.Position;
+
+        // Add some randomness to the direction.
+        direction += (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
+        mob.Rotation = direction;
+
+        // Choose the velocity.
+        var velocity = new Vector2((float)GD.RandRange(150.0, 250.0), 0);
+        mob.LinearVelocity = velocity.Rotated(direction);
+
+        // Spawn the mob by adding it to the Main scene.
+        AddChild(mob);
+    }
+
+}
--- src/scripts/Mob.cs
+++ src/scripts/Mob.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public class Mod : RigidBody2D
+public class Mob : RigidBody2D
 {
     public override void _Ready()
     {
